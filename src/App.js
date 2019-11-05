9daa69728d37dd5ea563c2f733932551c7ad71f9
nanut <galunyu@gmail.com>
2019-11-06 01:29:32 +0700

    finished

--- src/App.js
+++ src/App.js
@@ -1,60 +1,55 @@
 import React, { Component } from "react";
-import axios from 'axios'
+import { ToastContainer } from 'react-toastify'
+import http from './services/httpService'
+import config from './config.json'
+import "react-toastify/dist/ReactToastify.css"
 import "./App.css";
-
-const apiEndpoint = 'https://jsonplaceholder.typicode.com/posts?_limit=10'
-const apiEndpoint2 = 'https://jsonplaceholder.typicode.com/posts'
-
-axios.interceptors.response.use(null, error => {
-  const expectedError =
-    error.response &&
-    error.response.state >= 400 &&
-    error.response.status < 500
-  if (!expectedError) {
-    console.log("Logging the error", error)
-    alert("An unexpected error occurred")
-  }
-  // console.log('INTERCEPTOR CALLED')
-  return Promise.reject(error)
-})
 
 
 class App extends Component {
   state = {
-    posts: []
+    posts: [],
+    query: ''
   };
 
   async componentDidMount() {
     //? pending > resolved (sucess) or reject (failue)
-    // const promise = axios.get('https://jsonplaceholder.typicode.com/posts')
+    // const promise = http.get('https://jsonplaceholder.typicode.com/posts')
     // const response = await promise
     // console.log(response)
     //? refactor
-    const { data: posts } = await axios.get(apiEndpoint)
+    const { data: posts } = await http.get(config.apiEndpoint)
     this.setState({ posts })
   }
 
 
   handleAdd = async () => {
-    console.log("Add");
     const obj = { title: 'a', body: 'b' }
-    const { data: post } = await axios.post(apiEndpoint, obj)
+    const { data: post } = await http.post(config.apiEndpoint, obj)
 
     const posts = [post, ...this.state.posts]
     this.setState({ posts })
   };
 
   handleUpdate = async post => {
+    const originalPosts = this.state.posts
     post.title = "UPDATED"
-    // const { data } = await axios.put(apiEndpoint2 + '/' + post.id, post)
-    // axios.patch(apiEndpoint2 + '/' +post.id, {title: post.title})
+    // const { data } = await http.put(config.apiEndpoint2 + '/' + post.id, post)
+    // http.patch(config.apiEndpoint2 + '/' +post.id, {title: post.title})
     // console.log(data)
-
-    await axios.put(apiEndpoint2 + "/" + post.id, post)
     const posts = [...this.state.posts]
     const index = posts.indexOf(post)
     posts[index] = { ...post }
     this.setState({ posts })
+    try {
+      await http.put(config.apiEndpoint2 + "/" + post.id, post)
+      throw new Error("")
+    } catch (ex) {
+      if (ex.response && ex.response.status === 404)
+        alert('This post has already been deleted')
+      // alert("Something failed while deleting a post!")
+      this.setState({ posts: originalPosts })
+    }
   };
 
   handleDelete = async post => {
@@ -64,7 +59,8 @@ class App extends Component {
     this.setState({ posts })
 
     try {
-      await axios.delete("s" + apiEndpoint2 + '/' + post.id)
+      const result = await http.delete('s' + config.apiEndpoint2 + '/' + post.id)
+      console.log(result)
       throw new Error("")
     }
     catch (ex) {
@@ -76,6 +72,8 @@ class App extends Component {
     }
   };
 
+  handleQuery = () => { }
+
   //? ex.request, ex.reponse
   //? Expected (404: not found, 400: bad request) - CLIENT ERRORS
   //? - Display a specific error message
@@ -86,6 +84,13 @@ class App extends Component {
   render() {
     return (
       <React.Fragment>
+        <ToastContainer />
+        <input
+          type="text"
+          className="form-control col-6"
+          placeholder="Serch...."
+          value={ this.state.query }
+          onChange={ () => this.handleQuery() } /> <br />
         <button className="btn btn-primary my-3" onClick={ this.handleAdd }>
           Add
         </button>
--- src/components/searchBox.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const SearchBox = () => {
-  return (null);
-}
-
-export default SearchBox;
