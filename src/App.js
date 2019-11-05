10ceeb3934f636497b2f6a7a86a037a20db64f11
nanut <galunyu@gmail.com>
2019-11-05 22:21:20 +0700

    http service

--- src/App.js
+++ src/App.js
@@ -5,6 +5,20 @@ import "./App.css";
 const apiEndpoint = 'https://jsonplaceholder.typicode.com/posts?_limit=10'
 const apiEndpoint2 = 'https://jsonplaceholder.typicode.com/posts'
 
+axios.interceptors.response.use(null, error => {
+  const expectedError =
+    error.response &&
+    error.response.state >= 400 &&
+    error.response.status < 500
+  if (!expectedError) {
+    console.log("Logging the error", error)
+    alert("An unexpected error occurred")
+  }
+  // console.log('INTERCEPTOR CALLED')
+  return Promise.reject(error)
+})
+
+
 class App extends Component {
   state = {
     posts: []
@@ -44,11 +58,30 @@ class App extends Component {
   };
 
   handleDelete = async post => {
-    await axios.delete(apiEndpoint2 + '/' + post.id)
+    const originalPosts = this.state.posts
+
     const posts = this.state.posts.filter(p => p.id !== post.id)
     this.setState({ posts })
 
+    try {
+      await axios.delete("s" + apiEndpoint2 + '/' + post.id)
+      throw new Error("")
+    }
+    catch (ex) {
+      // console.log("HANDLE DELETE CATCH BLOCK")
+      if (ex.response && ex.response.status === 404)
+        alert('This post has already been deleted')
+      // alert("Something failed while deleting a post!")
+      this.setState({ posts: originalPosts })
+    }
   };
+
+  //? ex.request, ex.reponse
+  //? Expected (404: not found, 400: bad request) - CLIENT ERRORS
+  //? - Display a specific error message
+  //? Unexpected (network down, server down, db down, bug)
+  //? - Log them
+  //? - Display a generic and firenly error message
 
   render() {
     return (
